a4e748585f9b78ec705479ecd9f54188ac11b3b7
zahwaainnayya28 <84946818+zahwaainnayya28@users.noreply.github.com>
2021-07-20 15:43:48 +0700

    Update Controller Tiket

--- controller/ControllerTiket.java
+++ controller/ControllerTiket.java
@@ -1,180 +1,137 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package controller;
 
-import model.Connector;
-import model.Tiket;
-import view.FormTiket;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+package model;
+
+import java.awt.HeadlessException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-import javax.swing.table.DefaultTableModel;
 
-public class ControllerTiket implements ActionListener, MouseListener{
-    Tiket data;
-    FormTiket form;
+public class Tiket extends Connector{
+    private String kode_tk;
+    private String kelas;
+    private String harga;
+    private String des;
+    private String supp;
+    private String jns;
+
+    public String getKode_tk() {
+        return kode_tk;
+    }
+
+    public void setKode_tk(String kode_tk) {
+        this.kode_tk = kode_tk;
+    }
+
+
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+
     
-    public ControllerTiket(Tiket data, FormTiket form){
-        this.data = data;
-            this.form = form;
-            this.form.btnSimpan.addActionListener(this);
-            this.form.btnTambah.addActionListener(this);
-            this.form.btnEdit.addActionListener(this);
-            this.form.btnHapus.addActionListener(this);
-            this.form.tabelTiket.addMouseListener(this); 
+    
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
     }
     
-    public void KosongFormTiket(){
-            form.txtNoTiket.setText(null);
-            form.cmbKelas.setSelectedItem(null);
-            form.txtHarga.setText(null);
-            form.txtDes.setText(null);
-            form.txtSupp.setText(null);
-            form.txtJns.setSelectedItem(null);
-        }
-    public void TampilDataFormTiket(){
-            DefaultTableModel model = new DefaultTableModel();
-            model.addColumn("No");
-            model.addColumn("Nomor Tiket");
-            model.addColumn("Kelas");
-            model.addColumn("Harga");
-            model.addColumn("Destinasi");
-            model.addColumn("Supplier");
-            model.addColumn("Jenis Kendaraan");
-            
-            // Menampilkan data pada database ke dalam tabel
-            try{
-                int no=1;
-                String sql="SELECT * FROM tiket";
-                java.sql.Connection conn=(Connection)Connector.configDB();
-                java.sql.Statement stm=conn.createStatement();
-                java.sql.ResultSet res=stm.executeQuery(sql);
-                
-                while(res.next()){
-                    model.addRow(new Object[]{
-                        no++,
-                        res.getString(1),
-                        res.getString(2),
-                        res.getString(3),
-                        res.getString(4),
-                        res.getString(5),
-                        res.getString(6),
-                    });
-                }
-                form.tabelTiket.setModel(model);
-            }catch(SQLException e){
-                System.out.println("Error "+e.getMessage());
-            }
-        }
-
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        if(ae.getSource()==form.btnTambah){
-            KosongFormTiket();
-        }else if(ae.getSource()==form.btnSimpan){
-            data.setKode_tk(form.txtNoTiket.getText());
-            data.setKelas((String) form.cmbKelas.getSelectedItem());
-            data.setHarga(form.txtHarga.getText());
-            data.setDes(form.txtDes.getText());
-            data.setSupp(form.txtSupp.getText());
-            data.setJns((String) form.txtJns.getSelectedItem());
-            
-            try {
-                if(data.SimpanTiket(data)){
-                    JOptionPane.showMessageDialog(null, "Yeay!!! Berhasil Menyimpan");
-                    KosongFormTiket();
-                    TampilDataFormTiket();
-                }
-            }catch (SQLException ex){
-                JOptionPane.showMessageDialog(null, ex);
-            }
-        }else if(ae.getSource()==form.btnEdit){
-            data.setKode_tk(form.txtNoTiket.getText());
-            data.setKelas((String) form.cmbKelas.getSelectedItem());
-            data.setHarga(form.txtHarga.getText());
-            data.setDes(form.txtDes.getText());
-            data.setSupp(form.txtSupp.getText());
-            data.setJns((String) form.txtJns.getSelectedItem());
-            
-            try {
-                if(data.UpdateTiket(data)){
-                    JOptionPane.showMessageDialog(null, "Update Anda Berhasil");
-                    KosongFormTiket();
-                    TampilDataFormTiket();
-                }
-            }catch (SQLException ex){
-                JOptionPane.showMessageDialog(null, ex);
-            }
-        }else {
-            data.setKode_tk(form.txtNoTiket.getText());
-            
-            try {
-                if(data.HapusTiket(data)){
-                    JOptionPane.showMessageDialog(null, "Berhasil Menghapus");
-                    KosongFormTiket();
-                    TampilDataFormTiket();
-                }
-            }catch (SQLException ex){
-                JOptionPane.showMessageDialog(null, ex);
-            }
-        }
+    
+    
+    public String getDes() {
+        return des;
     }
 
-    @Override
-    public void mouseClicked(MouseEvent me) {
-        if(me.getSource()==form.tabelTiket){
-            form.txtNoTiket.setEditable(false);
-            int baris=form.tabelTiket.rowAtPoint(me.getPoint());
-            
-            String no_tiket=form.tabelTiket.getValueAt(baris, 1).toString();
-            form.txtNoTiket.setText(no_tiket);
-            
-            String kelas=form.tabelTiket.getValueAt(baris, 2).toString();
-            form.cmbKelas.setSelectedItem(kelas);
-            
-            String harga_tiket=form.tabelTiket.getValueAt(baris, 3).toString();
-            form.txtHarga.setText(harga_tiket);
-            
-            String des=form.tabelTiket.getValueAt(baris, 4).toString();
-            form.txtDes.setText(des);
-            
-            String supp=form.tabelTiket.getValueAt(baris, 5).toString();
-            form.txtSupp.setText(supp);
-            
-            String jns=form.tabelTiket.getValueAt(baris, 6).toString();
-            form.txtJns.setSelectedItem(jns);
-        }
+    public void setDes(String des) {
+        this.des = des;
+    }
+    
+    
+    public String getSupp() {
+        return supp;
     }
 
-    @Override
-    public void mousePressed(MouseEvent me) {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setSupp(String supp) {
+        this.supp = supp;
     }
 
-    @Override
-    public void mouseReleased(MouseEvent me) {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    
+    public String getJns() {
+        return jns;
     }
 
-    @Override
-    public void mouseEntered(MouseEvent me) {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setJns(String jns) {
+        this.jns = jns;
     }
-
-    @Override
-    public void mouseExited(MouseEvent me) {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    
+    public boolean SimpanTiket(Tiket data) throws SQLException{
+        PreparedStatement pstm=null;
+        Connection conn=(Connection)Connector.configDB();
         
+        String sql="INSERT INTO tiket (kode_tk, kelas, harga, des, supp, jns ) VALUE(?, ?, ?, ?, ?, ?)";
+        
+        try{
+            pstm=conn.prepareStatement(sql);
+            pstm.setString(1, data.getKode_tk());
+            pstm.setString(2, data.getKelas());
+            pstm.setString(3, data.getHarga());
+            pstm.setString(4, data.getDes());
+            pstm.setString(5, data.getSupp());
+            pstm.setString(6, data.getJns());
+            
+            pstm.execute();
+            return true;
+        }catch(HeadlessException | SQLException e){
+            System.err.println(e);
+            return false;
+        }
+    }
+    public boolean UpdateTiket(Tiket data) throws SQLException{
+        PreparedStatement pstm=null;
+        Connection conn=(Connection)Connector.configDB();
+        
+        String sql="UPDATE tiket SET kelas=?,harga=?,des=?,supp=?,jns=?  WHERE kode_tk=?";
+        
+        try{
+            pstm=conn.prepareStatement(sql);
+            pstm.setString(6, data.getKode_tk());
+            pstm.setString(1, data.getKelas());
+            pstm.setString(2, data.getHarga());
+            pstm.setString(3, data.getDes());
+            pstm.setString(4, data.getSupp());
+            pstm.setString(5, data.getJns());
+            pstm.execute();
+            return true;
+        }catch(HeadlessException | SQLException e){
+            System.err.println(e);
+            return false;
+        }
+    }
+    public boolean HapusTiket(Tiket data) throws SQLException{
+        PreparedStatement pstm=null;
+        Connection conn=(Connection)Connector.configDB();
+        
+        String sql="DELETE FROM tiket WHERE kode_tk=?";
+        
+        try{
+            pstm=conn.prepareStatement(sql);
+            pstm.setString(1, data.getKode_tk()); 
+            pstm.execute();
+            return true;
+        }catch(HeadlessException | SQLException e){
+            System.err.println(e);
+            return false;
+        }
+    }    
     
-     
+    
 }
